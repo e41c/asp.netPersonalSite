b78854f9f73beb7b6467237732a1098b00a16510
e41c <61212309+e41c@users.noreply.github.com>
2023-04-20 11:36:10 -0400

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+$(window).scroll(function () {
+    // Change the navigation bar background color on scroll
+    var nav = $("nav");
+    if ($(this).scrollTop() > nav.height()) {
+        nav.addClass("scrolled");
+    } else {
+        nav.removeClass("scrolled");
+    }
+});
